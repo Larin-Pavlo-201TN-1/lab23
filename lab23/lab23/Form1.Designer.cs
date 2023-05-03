0ec8cb598d422d1083f4a8c69887fcb63656e234
Larin-Pavlo-201TN-1 <98223565+Larin-Pavlo-201TN-1@users.noreply.github.com>
2023-05-03 22:22:50 +0300

    Add files via upload

---
+++ lab23/lab23/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace lab23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.Diagram = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.gridA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gridB = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gridT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.Diagram)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Diagram
+            // 
+            chartArea1.AxisX.Title = "x";
+            chartArea1.AxisX.TitleAlignment = System.Drawing.StringAlignment.Far;
+            chartArea1.AxisY.Title = "y";
+            chartArea1.AxisY.TitleAlignment = System.Drawing.StringAlignment.Far;
+            chartArea1.Name = "ChartArea1";
+            this.Diagram.ChartAreas.Add(chartArea1);
+            legend1.AutoFitMinFontSize = 10;
+            legend1.Name = "Legend1";
+            legend1.Title = "Графік";
+            legend1.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.Diagram.Legends.Add(legend1);
+            this.Diagram.Location = new System.Drawing.Point(277, 12);
+            this.Diagram.Name = "Diagram";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.SystemColors.HotTrack;
+            series1.Legend = "Legend1";
+            series1.Name = "Фунція";
+            this.Diagram.Series.Add(series1);
+            this.Diagram.Size = new System.Drawing.Size(770, 462);
+            this.Diagram.TabIndex = 0;
+            this.Diagram.Text = "chart1";
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            title1.Name = "Title1";
+            title1.Text = "Графік функції";
+            this.Diagram.Titles.Add(title1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::lab23.Properties.Resources.Снимок_экрана__384_;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(259, 56);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.BackgroundColor = System.Drawing.Color.LightCyan;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.gridA,
+            this.gridB,
+            this.gridT});
+            this.dataGrid.Location = new System.Drawing.Point(12, 74);
+            this.dataGrid.Name = "dataGrid";
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGrid.RowHeadersVisible = false;
+            this.dataGrid.RowHeadersWidth = 51;
+            this.dataGrid.RowTemplate.Height = 24;
+            this.dataGrid.Size = new System.Drawing.Size(257, 337);
+            this.dataGrid.TabIndex = 2;
+            this.dataGrid.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.dataGrid_EditingControlShowing);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightCyan;
+            this.button1.Location = new System.Drawing.Point(12, 417);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(259, 57);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Побудувати графік функції";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // gridA
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.gridA.DefaultCellStyle = dataGridViewCellStyle1;
+            this.gridA.HeaderText = "a";
+            this.gridA.MinimumWidth = 40;
+            this.gridA.Name = "gridA";
+            this.gridA.Width = 60;
+            // 
+            // gridB
+            // 
+            this.gridB.HeaderText = "b";
+            this.gridB.MinimumWidth = 40;
+            this.gridB.Name = "gridB";
+            this.gridB.Width = 60;
+            // 
+            // gridT
+            // 
+            this.gridT.HeaderText = "t(градуси)";
+            this.gridT.MinimumWidth = 40;
+            this.gridT.Name = "gridT";
+            this.gridT.Width = 70;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1059, 487);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Diagram);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Diagram)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart Diagram;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gridA;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gridB;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gridT;
+    }
+}
+
---
+++ lab23/lab23/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab23
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGrid_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            DataGridViewTextBoxEditingControl tb = (DataGridViewTextBoxEditingControl)e.Control;
+            tb.KeyPress += new KeyPressEventHandler(dataGrid_KeyPress);
+            e.Control.KeyPress += new KeyPressEventHandler(dataGrid_KeyPress);
+
+        }
+        private void dataGrid_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= '0') && (e.KeyChar <= '9'))
+            {
+                return;
+            }
+            if (e.KeyChar == (char)Keys.Back)
+            {
+                return;
+            }
+            e.Handled = true;
+            return;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x, y;
+            int a, b, t;
+            int[,] arr = new int[dataGrid.Rows.Count, dataGrid.ColumnCount];
+            for(int i = 0; i < dataGrid.Rows.Count; i++)
+            {
+                for(int j = 0; j < dataGrid.ColumnCount; j++)
+                {
+                    arr[i, j] = Convert.ToInt32(dataGrid[j, i].Value);
+                }
+            }
+            Diagram.Series[0].Points.Clear();
+            for (int i = 0; i < dataGrid.Rows.Count; i++)
+            {
+                a = arr[i, 0];
+                b = arr[i, 1];
+                t = arr[i, 2];
+                x = a * Math.Cos(t * Math.PI / 180) * (Math.Cos(t * Math.PI / 180) + b);
+                y = Math.Sin(t * Math.PI / 180) * (Math.Sin(t * Math.PI / 180) + b);
+                Diagram.Series[0].Points.AddXY(x, y);
+            }
+        }
+    }
+}
